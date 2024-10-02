951b933d597695292c179c55b902f158680a1456
Shubh7Codes <shubh7codes@gmail.com>
2024-10-02 18:28:01 +0530

    final merge

--- app/(routes)/dashboard/_components/BarChartDashBoard.jsx
+++ app/(routes)/dashboard/_components/BarChartDashBoard.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import { Bar, BarChart, Legend, Tooltip, XAxis, YAxis } from 'recharts'
+import React from 'react';
+import { Bar, BarChart, Legend, Tooltip, XAxis, YAxis } from 'recharts';
 
-function BarChartDashBoard({budgetList}) {
+function BarChartDashBoard({ budgetList = [] }) {
   return (
     <div className='border rounded-lg p-5'>
       <h2 className='font-bold text-lg'>Activity</h2>
       <BarChart
-      width={500}
-      height={300}
-      data={budgetList}
-      margin={{
-        top:5,
-        right:5,
-        left:5,
-        bottom:5
-      }}
+        width={500}
+        height={300}
+        data={budgetList}
+        margin={{
+          top: 5,
+          right: 5,
+          left: 5,
+          bottom: 5,
+        }}
       >
-        <XAxis dataKey='name'/>
+        <XAxis dataKey='name' />
         <YAxis />
-        <Tooltip/>
-        <Legend/>
-        <Bar dataKey='totalSpend' stackId='a' fill='#4845d2'/>
-        <Bar dataKey='amount' stackId='a' fill='#c3c2ff'/>
+        <Tooltip />
+        <Legend />
+        <Bar dataKey='totalSpend' stackId='a' fill='#4845d2' />
+        <Bar dataKey='amount' stackId='a' fill='#c3c2ff' />
       </BarChart>
     </div>
-  )
+  );
 }
 
-export default BarChartDashBoard
+export default BarChartDashBoard;
